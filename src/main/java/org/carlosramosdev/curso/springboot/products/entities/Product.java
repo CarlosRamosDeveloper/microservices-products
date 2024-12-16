8b34e4eedc5b34b98ce3e7cd34ab011475256148
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-12-16 12:07:20 +0100

    Product entity creation

---
+++ src/main/java/org/carlosramosdev/curso/springboot/products/entities/Product.java
@@ -0,0 +1,64 @@
+package org.carlosramosdev.curso.springboot.products.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="productos")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="nombre")
+    private String name;
+    @Column(name="precio")
+    private Double price;
+    @Column(name="descripcion")
+    private String description;
+    @Column(name="Fecha de creacion")
+    private LocalDate createAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+}
