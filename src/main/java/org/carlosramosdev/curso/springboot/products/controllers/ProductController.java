4d410b9fcc0c0f7bcac4a943576536d1cfce9e94
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-12-16 15:10:43 +0100

    Added request mapping in controller

--- src/main/java/org/carlosramosdev/curso/springboot/products/controllers/ProductController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/products/controllers/ProductController.java
@@ -6,12 +6,14 @@ import org.carlosramosdev.curso.springboot.products.services.ProductServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/products")
 public class ProductController {
     final IProductService service;
 
